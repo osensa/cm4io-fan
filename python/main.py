e0fddc0f11e8b3b954a38b2aa46069ca9d43a1e1
sjpark608 <spark@osensa.com>
2021-10-29 15:07:24 -0700

    Update main.py
    simple and dirty python script

--- python/main.py
+++ python/main.py
@@ -1,16 +1,56 @@
-# This is a sample Python script.
+#!/usr/bin/env python3
+import sys
+from types import ModuleType
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from i2c_pkg import i2c as i2c_module
+from i2c_pkg.emc2301_pkg import emc2301
 
 
-# Press the green button in the gutter to run the script.
+class RaspiCM4IOBoardFanSensor(emc2301.EMC2301):
+    def __init__(self,
+                 address: int = 0x2f,
+                 busnum: int = 10,
+                 i2c: ModuleType = i2c_module,
+                 **kwargs):
+        super().__init__(address=address,
+                         busnum=busnum,
+                         i2c=i2c,
+                         **kwargs)
+
+    def fan_speed(self) -> int:
+        return self.speed()[0]
+
+    def set_fan_speed_percentage(self, percentage: int):
+        """Percentage should be 0 - 100"""
+        converted_value = int(percentage / 100 * 255)
+        self.write_register(register='FAN_SETTING', value=converted_value)
+
+
+def print_help():
+    print(f'Supported commands:')
+    print(f'{sys.argv[0]} set (speed in percentage)')
+    print(f'{sys.argv[0]} get')
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    if len(sys.argv) <= 1:
+        print_help()
+        sys.exit(1)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    fan = RaspiCM4IOBoardFanSensor()
+    if sys.argv[1] == 'set':
+        print(f'Setting fan speed to {sys.argv[2]}%')
+        try:
+            fan.set_fan_speed_percentage(int(sys.argv[2]))
+        except ValueError:
+            print(f'Incorrect speed percentage value: {sys.argv[2]}')
+            sys.exit(1)
+        sys.exit(0)
+
+    if sys.argv[1] == 'get':
+        print(f'Current fan speed: {fan.fan_speed()} RPM')
+        sys.exit(0)
+
+    print(f'Unknown command.')
+    print_help()
+    sys.exit(1)
